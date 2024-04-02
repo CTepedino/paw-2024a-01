d2cc1642de794a068ab9a66e0d5f403c4604732a
juanamendonca <108553386+juanamendonca@users.noreply.github.com>
2024-04-02 17:13:49 -0300

    Writer model and service

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/WriterDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/WriterDao.java
@@ -1,2 +1,11 @@
-package ar.edu.itba.paw.interfaces;public interface WriterDao {
+package ar.edu.itba.paw.interfaces;
+
+import ar.edu.itba.paw.models.Writer;
+
+import java.util.Optional;
+
+public interface WriterDao {
+    Optional<Writer> findById(long id);
+
+    Writer create(String name, String lastName, String email);
 }
--- services/src/main/java/ar/edu/itba/paw/services/WriterServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/WriterServiceImpl.java
@@ -1,2 +1,32 @@
-package ar.edu.itba.paw.services;public class WriterServiceImpl {
+package ar.edu.itba.paw.services;
+
+
+import ar.edu.itba.paw.interfaces.WriterDao;
+import ar.edu.itba.paw.interfaces.WriterService;
+import ar.edu.itba.paw.models.Writer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+public class WriterServiceImpl implements WriterService {
+
+    private final WriterDao writerDao;
+
+    @Autowired
+    public WriterServiceImpl(final WriterDao writerDao){
+        this.writerDao=writerDao;
+    }
+
+    @Override
+    public Optional<Writer> findById(long id) {
+        return writerDao.findById(id);
+    }
+
+    @Override
+    public Writer create(String name, String lastName, String email) {
+        return writerDao.create(name, lastName, email);
+    }
 }
