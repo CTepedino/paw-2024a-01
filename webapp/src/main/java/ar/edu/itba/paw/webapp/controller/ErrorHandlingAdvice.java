c9b44d75208421ded2f7c022f5810af362b7dd3a
CTepedino <ctepedino@itba.edu.ar>
2024-05-13 00:48:52 -0300

    Added maxUploadSizeExceeded exception handler

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorHandlingAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorHandlingAdvice.java
@@ -2,55 +2,78 @@ package ar.edu.itba.paw.webapp.controller;
 
 
 import ar.edu.itba.paw.models.exception.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ErrorHandlingAdvice {
 
     @ExceptionHandler(BookNotFoundException.class)
-    public ModelAndView bookNotFound(){return new ModelAndView("exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView bookNotFound(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(IllegalReviewException.class)
-    public ModelAndView illegalReview(){return new ModelAndView("exception/400");}
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ModelAndView illegalReview(){return new ModelAndView("forward:/400");}
 
     @ExceptionHandler(IllegalSearchQueryException.class)
-    public ModelAndView illegalSearchQuery(){return new ModelAndView("exception/400");}
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ModelAndView illegalSearchQuery(){return new ModelAndView("forward:/400");}
 
     @ExceptionHandler(ImageNotFoundException.class)
-    public ModelAndView imageNotFound(){return new ModelAndView("/exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView imageNotFound(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(InvalidCodeException.class)
-    public ModelAndView invalidCode(){return new ModelAndView("exception/403");}
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ModelAndView invalidCode(){return new ModelAndView("forward:/403");}
 
     @ExceptionHandler(InvalidOrderUpdateException.class)
-    public ModelAndView invalidOrderUpdate(){return new ModelAndView("exception/400");}
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ModelAndView invalidOrderUpdate(){return new ModelAndView("forward:/400");}
 
     @ExceptionHandler(InvalidPageException.class)
-    public ModelAndView invalidPage(){return new ModelAndView("/exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView invalidPage(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(NoValidationCodeException.class)
-    public ModelAndView noValidationCode(){return new ModelAndView("exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView noValidationCode(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(OrderAlreadyExistsException.class)
-    public ModelAndView orderExists(){return new ModelAndView("/exception/403");}
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ModelAndView orderExists(){return new ModelAndView("forward:/403");}
 
     @ExceptionHandler(OrderNotFoundException.class)
-    public ModelAndView orderNotFound(){return new ModelAndView("/exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView orderNotFound(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(PdfNotFoundException.class)
-    public ModelAndView pdfNotFound(){return new ModelAndView("/exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView pdfNotFound(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(SameWriterAndBuyerException.class)
-    public ModelAndView buyingOwnBook(){return new ModelAndView("/exception/403");}
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ModelAndView buyingOwnBook(){return new ModelAndView("forward:/403");}
 
     @ExceptionHandler(UnreadableFileException.class)
-    public ModelAndView unreadableFile(){return new ModelAndView("/exception/400");}
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ModelAndView unreadableFile(){return new ModelAndView("forward:/400");}
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ModelAndView userNotFound(){return new ModelAndView("exception/404");}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView userNotFound(){return new ModelAndView("forward:/404");}
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionCatchAll(){return new ModelAndView("exception/500");}
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView exceptionCatchAll(){return new ModelAndView("forward:/500");}
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ModelAndView maxUploadSizeExceeded() {return new ModelAndView("forward:/400");}
+
 }
