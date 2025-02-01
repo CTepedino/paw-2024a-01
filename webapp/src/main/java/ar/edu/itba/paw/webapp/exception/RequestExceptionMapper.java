1a333aaf07c0f49669f25139f1430d742dc55df0
CTepedino <ctepedino@itba.edu.ar>
2025-02-01 17:09:34 -0300

    RequestException mapper

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exception/RequestExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exception/RequestExceptionMapper.java
@@ -1,4 +1,22 @@
 package ar.edu.itba.paw.webapp.exception;
 
-public class RequestExceptionMapper {
+import ar.edu.itba.paw.models.exception.RequestException;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Singleton
+@Provider
+@Component
+public class RequestExceptionMapper implements ExceptionMapper<RequestException> {
+
+    @Override
+    public Response toResponse(RequestException e) {
+        return Response
+                .status(e.getStatusCode())
+                .build();
+    }
 }
