4a953e5c0e29fdca9dca958c805500a5c933ee56
LucaBloise <lbloise@itba.edu.ar>
2025-02-11 04:00:03 -0300

    Added filter buttons

--- frontend/src/app/pages/sales/sales.component.ts
+++ frontend/src/app/pages/sales/sales.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
+
+export enum OrderStatus {
+  All = 'All status',
+  Rejected = 'Rejected payment',
+  Approval = 'Approval needed',
+  Completed = 'Completed',
+}
 
 @Component({
   selector: 'app-sales',
-  imports: [],
+  imports: [MatGridListModule, MatFormFieldModule, MatLabel, MatInputModule, MatSelectModule, FormsModule],
   templateUrl: './sales.component.html',
-  styleUrl: './sales.component.scss'
+  styleUrl: './sales.component.scss',
 })
 export class SalesComponent {
-
+  selectedStatus: OrderStatus = OrderStatus.All;
+  statusValues = Object.values(OrderStatus);
 }
