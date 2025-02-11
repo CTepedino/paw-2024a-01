56a8c6fd74d96706d019e32506a7a52b0b6ada24
LucaBloise <lbloise@itba.edu.ar>
2025-02-11 15:16:37 -0300

    Improved filter buttons

--- frontend/src/app/pages/sales/sales.component.ts
+++ frontend/src/app/pages/sales/sales.component.ts
@@ -4,13 +4,8 @@ import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {FormsModule} from "@angular/forms";
+import {OrderStatus} from "../../shared/model/order/orderStatus";
 
-export enum OrderStatus {
-  All = 'All status',
-  Rejected = 'Rejected payment',
-  Approval = 'Approval needed',
-  Completed = 'Completed',
-}
 
 @Component({
   selector: 'app-sales',
@@ -19,6 +14,12 @@ export enum OrderStatus {
   styleUrl: './sales.component.scss',
 })
 export class SalesComponent {
-  selectedStatus: OrderStatus = OrderStatus.All;
-  statusValues = Object.values(OrderStatus);
+  orderStatusOptions = [
+    { label: $localize`All status`, value: 'ALL' },
+    { label: $localize`Rejected payment`, value: OrderStatus.REJECTED_PAYMENT },
+    { label: $localize`Approval needed`, value: OrderStatus.WAITING_APPROVAL },
+    { label: $localize`Completed`, value: OrderStatus.COMPLETED }
+  ];
+
+  selectedStatus = 'ALL';
 }
