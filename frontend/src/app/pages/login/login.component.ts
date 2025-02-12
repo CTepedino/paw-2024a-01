4767c28f8dc98970aa7cf62dac172dc3c28381b1
CTepedino <ctepedino@itba.edu.ar>
2025-02-12 01:08:08 -0300

    Correctly reset form on validation error

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -49,7 +49,7 @@ export class LoginComponent {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
-    rememberMe: new FormControl(false, [Validators.required])
+    rememberMe: new FormControl(false)
   });
 
   doLogin(){
