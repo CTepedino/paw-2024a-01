1a50b01927e674800a7822cdfe6f06b9589d48fb
LucaBloise <lbloise@itba.edu.ar>
2025-02-14 19:53:47 -0300

    Sales View Finished

---
+++ frontend/src/app/pages/sales/components/accept-popup/accept-popup.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-accept-popup',
+  imports: [
+      MatFormFieldModule,
+      MatInputModule,
+      FormsModule,
+      MatButtonModule,
+      MatDialogTitle,
+      MatDialogContent,
+      MatDialogActions,
+      MatDialogClose],
+  templateUrl: './accept-popup.component.html',
+  styleUrl: './accept-popup.component.scss'
+})
+export class AcceptPopupComponent {
+  constructor(public dialogRef: MatDialogRef<AcceptPopupComponent>) { }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
