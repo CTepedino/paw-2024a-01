f81a4c6bd01ba054b79c2bb9fdf3c84e0e0519e2
LucaBloise <lbloise@itba.edu.ar>
2025-02-16 04:42:57 -0300

    Analytics View Finished

--- frontend/src/app/pages/analytics/analytics.component.ts
+++ frontend/src/app/pages/analytics/analytics.component.ts
@@ -4,14 +4,27 @@ import {TabComponent} from "../../shared/components/tab/tab.component";
 import {WriterCategory} from "../../shared/model/user/writerCategory";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {CurrencyPipe} from "@angular/common";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
 
 @Component({
   selector: 'app-analytics',
-  imports: [TabComponent, MatGridListModule, CurrencyPipe],
+  imports: [TabComponent, MatGridListModule, CurrencyPipe, MatCheckboxModule, FormsModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
 export class AnalyticsComponent {
+  showByMonth = false;
+  selectedMonth: string = 'February';
+  selectedYear: number = 2025;
+
+  years = [2024, 2025];
+  months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
   user = {
     writerCategory: WriterCategory.DEFAULT,
@@ -23,5 +36,17 @@ export class AnalyticsComponent {
     ordersThisMonth: 3,
     revenueThisMonth: 150,
   }
+
+  books = [{
+    book: {
+      id: 1,
+      title: 'Test Book',
+      author: 'Luca Bloise',
+      price: 399,
+      coverUrl: 'assets/book-cover.jpg'
+    },
+    totalOrders: 3,
+    totalRevenue: 100,
+  }];
 
 }
